aa0f63cebe54b7cb2311d53d73911cd4b191b8e2
ducanhpn <phamngocducanh19@gmail.com>
2023-12-15 17:39:55 +0100

    Detect Ground

--- player.gd
+++ player.gd
@@ -2,9 +2,17 @@ extends RigidBody2D
 
 var force = Vector2(0,-70)
 
+func _ready():
+	contact_monitor = true
+	max_contacts_reported = 2
+
 func _integrate_forces(state):
 	if Input.is_action_pressed("Jump_Up"):
-		print("Pressed")
 		self.apply_central_impulse(force)
 	
 		
+
+
+func _on_body_entered(body):
+	if body.name == "Ground":
+		print("Game Over")
